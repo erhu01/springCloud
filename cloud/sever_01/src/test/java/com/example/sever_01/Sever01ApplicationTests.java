893c7eb4f0447f5bb6afcdf212ed822da3799290
erhu01 <72592782+erhu01@users.noreply.github.com>
2020-10-15 10:41:30 +0800

    Add files via upload

---
+++ cloud/sever_01/src/test/java/com/example/sever_01/Sever01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sever_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sever01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
