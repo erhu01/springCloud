41ecdd857e6e5b64e5e770a75424dcd0e779ab83
张一雄 <381173411@qq.com>
2020-10-16 15:25:05 +0800

    config获取git上的配置文件

--- cloud/config_01/src/main/java/com/example/config_01/Config01Application.java
+++ cloud/config_01/src/main/java/com/example/config_01/Config01Application.java
@@ -1,9 +1,15 @@
 package com.example.config_01;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableConfigServer
+@SpringBootConfiguration
 public class Config01Application {
 
 	public static void main(String[] args) {
