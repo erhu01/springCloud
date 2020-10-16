0adffe370182ef6eaf9cf45f089e3548e41f630b
张一雄 <381173411@qq.com>
2020-10-16 13:33:19 +0800

    zyx

---
+++ cloud/config_01/src/main/java/com/example/config_01/Config01Application.java
@@ -0,0 +1,13 @@
+package com.example.config_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Config01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Config01Application.class, args);
+	}
+
+}
